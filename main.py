30280547fcb82be063041ab5b35fa7d3b919e5ac
Shantha Kumar <mailtoshan265@gmail.com>
2023-11-21 15:31:10 +0530

    Added date and line break.

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ for filepath in filepaths:
 
     pdf = FPDF(orientation='p', unit='mm', format='A4')
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split('-')[0]
-    pdf.set_font(family='Times', size=16,style='B')
-    pdf.cell(w=50, h=8, txt=f'Invoice No.{invoice_nr}')
+    invoice_nr, date = filename.split('-')
+
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f'Invoice No.{invoice_nr}',ln=1)
+
+    pdf.set_font(family='Times', size=15, style='B')
+    pdf.cell(w=50, h=8, txt=f'Date {date}',ln=1)
+
     pdf.output(f"PDFs/{invoice_nr}.pdf")
